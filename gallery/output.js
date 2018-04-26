5f9e2ca9ce0fd20aa2142c578344370bb289fe29
wangcch <ci713@qq.com>
2018-04-27 02:19:45 +0800

    Site updated: 2018-04-27 02:19:44

---
+++ gallery/output.js
@@ -0,0 +1,62 @@
+[
+	{
+		"title": "demo12",
+		"date": "2018/11/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-11-12-demo12.jpg"
+	},
+	{
+		"title": "demo11",
+		"date": "2018/10/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-10-12-demo11.jpg"
+	},
+	{
+		"title": "demo10",
+		"date": "2018/09/16",
+		"url": "https://cdn.wangcch.cc/blog/2018-09-16-demo10.jpg"
+	},
+	{
+		"title": "demo09",
+		"date": "2018/08/16",
+		"url": "https://cdn.wangcch.cc/blog/2018-08-16-demo09.jpg"
+	},
+	{
+		"title": "demo08",
+		"date": "2018/07/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-07-12-demo08.jpg"
+	},
+	{
+		"title": "demo07",
+		"date": "2018/06/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-06-12-demo07.jpg"
+	},
+	{
+		"title": "demo06",
+		"date": "2018/05/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-05-12-demo06.jpg"
+	},
+	{
+		"title": "demo05",
+		"date": "2018/05/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-05-12-demo05.jpg"
+	},
+	{
+		"title": "demo04",
+		"date": "2018/04/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-04-12-demo04.jpg"
+	},
+	{
+		"title": "demo03",
+		"date": "2018/03/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-03-12-demo03.jpg"
+	},
+	{
+		"title": "demo02",
+		"date": "2018/02/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-02-12-demo02.jpg"
+	},
+	{
+		"title": "demo01",
+		"date": "2018/01/12",
+		"url": "https://cdn.wangcch.cc/blog/2018-01-12-demo01.jpg"
+	}
+]
--- gallery/tools.js
+++ gallery/tools.js
@@ -1,23 +1,51 @@
 const fs = require("fs");
-const path = "../../gallery";
+const path = '../../gallery';
+const cdnUrl = 'https://cdn.wangcch.cc/blog/'
+
+const _formatName = (name) => {
+  let data = name;
+  data = data.replace('.jpg', '');
+  data = data.replace('.png', '');
+  data = data.replace('.jpeg', '');
+  data = data.replace('.gif', '');
+  return data;
+}
+
+const _formateDate = (arr) => {
+  let date = '';
+  for (let i = 0; i < 3; i++) {
+    date += arr[i] + (i < 2 ? '/' : '');
+  }
+  return date
+}
+
 fs.readdir(path, function (err, files) {
-    if (err) {
-        return;
+  if (err) {
+    return;
+  }
+  let arr = [];
+  (function iterator(index) {
+    if (index == files.length) {
+      let reArr = arr.reverse();
+      fs.writeFile("output.js", JSON.stringify(reArr, null, "\t"));
+      return;
     }
-    let arr = [];
-    (function iterator(index) {
-        if (index == files.length) {
-            fs.writeFile("output.json", JSON.stringify(arr, null, "\t"));
-            return;
+    fs.stat(path + "/" + files[index], function (err, stats) {
+      if (err) {
+        return;
+      }
+      if (stats.isFile()) {
+        let cutArr = files[index].split('-');
+        if (cutArr.length > 3) {
+          cutArr[cutArr.length - 1] = _formatName(cutArr[cutArr.length - 1]);
+          let Obj = {};
+          Obj.title = cutArr[3];
+          Obj.date = _formateDate(cutArr);
+          Obj.url = cdnUrl + files[index];
+          arr.push(Obj);
         }
-        fs.stat(path + "/" + files[index], function (err, stats) {
-            if (err) {
-                return;
-            }
-            if (stats.isFile()) {
-                arr.push(files[index]);
-            }
-            iterator(index + 1);
-        })
-    }(0));
+      }
+      iterator(index + 1);
+    })
+  }(0));
 });
